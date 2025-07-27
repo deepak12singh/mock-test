6620213fd80a8ca48be14528abd69f5ec6b9c9eb
Deepak singh  <147910857+deepak12singh@users.noreply.github.com>
2025-07-27 14:57:26 -0700

    Update urls.py

--- Mock_Test/Mock_Test/urls.py
+++ Mock_Test/Mock_Test/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin-deepak/', admin.site.urls),
     path('users/', include('users.urls')),
     path('', include('core.urls')),
     path('', include('about.urls')),
